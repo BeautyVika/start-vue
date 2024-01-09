b7165133aef755f5ed59ce1eb43e85b611e24fc2
BeautyVika <vikachk54@gmail.com>
2024-01-09 14:52:11 +0300

    add sorting and animation

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
           @create="createPost"/>
     </my-dialog>
     <post-list
-        :posts="posts"
+        :posts="sortedPosts"
         @remove="removePost"
         v-if="!isPostLoading"
     />
@@ -71,6 +71,14 @@ export default {
   },
   mounted() {
     this.fetchPosts()
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+    }
+  },
+  watch: {
+
   }
 }
 </script>
